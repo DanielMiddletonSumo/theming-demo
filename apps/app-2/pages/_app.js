ab99e6fb43a8f65d8a807c9d019579c3c7c8a83e
Daniel Middleton <dmiddleton@sumologic.com>
2021-12-13 21:38:18 -0600

    Use MUI 4 and 5 in one app

--- apps/app-2/pages/_app.js
+++ apps/app-2/pages/_app.js
@@ -1,27 +1,14 @@
-import { useEffect } from 'react';
 import { theme } from 'theme-2';
-import {
-  ThemeProvider as ThemeProviderV5,
-  useTheme as useThemeV5,
-} from '@mui/material';
+import { ThemeProvider as ThemeProviderV5 } from 'heading';
+import { ThemeProvider } from '@material-ui/core';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    console.log('app 2 theme: ', theme);
-
-    // @ts-ignore
-    window.app2 = {
-      // ThemeProvider,
-      // useTheme,
-      ThemeProviderV5,
-      useThemeV5,
-    };
-  }, []);
-
   return (
-    <ThemeProviderV5 theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProviderV5>
+    <ThemeProvider theme={theme}>
+      <ThemeProviderV5 theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProviderV5>
+    </ThemeProvider>
   );
 }
 
